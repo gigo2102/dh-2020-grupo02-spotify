d132dba65538bd17468d9ed600492edf1a051fbe
Gisela Caputo <gigo2102@gmail.com>
2020-07-09 21:26:05 -0300

    setup inicial de git para probar streamlit

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"gigo2102@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
+echo "[general]" >> ~/.streamlit/credentials.toml
+echo "email = \"gigo2102@gmail.com\"" >> ~/.streamlit/credentials.toml
+echo "[server]" >> ~/.streamlit/config.toml
+echo "headless = true" >> ~/.streamlit/config.toml
+echo "enableCORS=false" >> ~/.streamlit/config.toml
+echo "port = $PORT" >> ~/.streamlit/config.toml
